b82857492cb4f74a8153dd6cfd493e8da90d3a47
soon10000 <ksm3925@naver.com>
2024-08-19 17:59:29 +0900

    0818 18:00 push

---
+++ golfro/src/main/java/com/itwill/golfro/config/AmazoneS3Config.java
@@ -0,0 +1,20 @@
+package com.itwill.golfro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class AmazoneS3Config {
+	
+	@Bean
+	public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion("YOUR_AWS_REGION") // 예: us-east-1
+                .build();
+    }
+	
+
+}
