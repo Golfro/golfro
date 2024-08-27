e252d90a99cacd14db6462df2d90a495feb1f27d
LJY <leejyeyoung@naver.com>
2024-08-27 16:20:26 +0900

    헤더/시큐리티

--- golfro/src/main/java/com/itwill/golfro/config/SecurityConfig.java
+++ golfro/src/main/java/com/itwill/golfro/config/SecurityConfig.java
@@ -3,22 +3,25 @@ package com.itwill.golfro.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-// import com.itwill.golfro.service.UserDetailService;
+import com.itwill.golfro.service.UserDetailService;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
 	@Autowired
-	// private UserDetailService userService;
+	@Lazy
+	private UserDetailService userService;
 
 	@Bean
+	@Lazy
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
@@ -27,7 +30,7 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf((csrf) -> csrf.disable());
 
-		// http.userDetailsService(userService);
+		http.userDetailsService(userService);
 
 		http.formLogin((login) -> login.loginPage("/user/signin").defaultSuccessUrl("/", true)
 				.failureUrl("/user/signin?error=true"));
