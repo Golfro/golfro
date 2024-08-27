dc85c49901b29df1951ec95e8eeb33ccec3f368f
LJY <leejyeyoung@naver.com>
2024-08-27 16:26:35 +0900

    시큐리티/헤더

--- golfro/src/main/java/com/itwill/golfro/config/SecurityConfig.java
+++ golfro/src/main/java/com/itwill/golfro/config/SecurityConfig.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-<<<<<<< HEAD
-=======
 import org.springframework.security.config.Customizer;
->>>>>>> refs/heads/LJY
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
