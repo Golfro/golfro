8180e4f7759c7cbf5eb74832e4d011471529b764
LJY <leejyeyoung@naver.com>
2024-09-05 18:17:23 +0900

    0905 review

--- golfro/src/main/java/com/itwill/golfro/web/ReviewController.java
+++ golfro/src/main/java/com/itwill/golfro/web/ReviewController.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
